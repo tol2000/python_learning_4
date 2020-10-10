f67b3fcd4d32c71859d7075544017059d0b7a528
tol2000 <tolicheg@gmail.com>
2020-10-10 20:50:13 +0300

    test simple client through deva)

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 # Main module
 
+import http.client
+
 if __name__ == '__main__':
-    print('Hi!')
+
+    h = http.client.HTTPConnection('192.168.3.19', 7778)
+    h.request('GET', '/prometheus_metrics/sessions')
+    resp = str(h.getresponse().read())
+    sl = resp.split('\\n')
+    for s in sl:
+        print(s)
+    print(sl)

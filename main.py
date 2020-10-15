80e2df5e121f77a64dd1525f4e23828e43ad7ffb
tol2000 <tolicheg@gmail.com>
2020-10-15 22:04:21 +0300

    -

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 # Main module
 
-import http.client
+# import http.client
+from pprint import pprint
+import requests
 
 if __name__ == '__main__':
 
-    h = http.client.HTTPConnection('192.168.3.19', 7778)
-    h.request('GET', '/prometheus_metrics/sessions')
-    resp = str(h.getresponse().read())
-    sl = resp.split('\\n')
-    for s in sl:
-        print(s)
-    print(sl)
+    # h = http.client.HTTPConnection('192.168.3.19', 7778)
+    # h.request('GET', '/prometheus_metrics/sessions')
+    # resp = str(h.getresponse().read())
+    # sl = resp.split('\\n')
+    # for s in sl:
+    #     print(s)
+    # print(sl)
+
+    # resp = requests.request('GET', 'http://192.168.3.19:7778/prometheus_metrics/sessions')
+    # print(resp)
+    # print("------------------------------------")
+    # sl = resp.text.split('\n')
+    # for s in sl:
+    #     print(s)
+    # print("------------------------------------")
+
+    # resp = requests.get('http://httpbin.org/get')
+    # pprint(resp.json())
+
+    resp = requests.post('http://httpbin.org/post')
+    pprint(resp.json())

37d709d4967cd79c1181602d0c2f03e48b9fc717
tol2000 <tolicheg@gmail.com>
2020-10-20 23:08:36 +0300

    lesson4, flask, Tolyan mega-server with get, post and ryushe4ki :)

--- web1.py
+++ web1.py
@@ -1,30 +1,55 @@
-from flask import Flask, render_template
-
-
-def page():
-    return render_template("index.html", pages=pages)
-
-
-def hello_name(name=None):
-    return render_template("hello.html", name=name)
-
-
-def hi_name():
-    return hello_name()
-
-
-pages = {
-    "/": (page,),
-    "/hello/": (hello_name,),
-    "/hello/<name>/": (hello_name,),
-    "/hi/": (hello_name,)
-}
-
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-for path in pages.keys():
-    app.add_url_rule(rule=path, view_func=pages[path][0])
+pages = {
+    "/",
+    "/hello/",
+    "/hello/Tolicheg/",
+    "/article/",
+    "/article/42/",
+    "/user/"
+}
+
+
+@app.route('/')
+def page():
+    return render_template('index.html', pages=pages)
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>/')
+def hello_name(name=None):
+    return render_template('hello.html', name=name)
+
+
+@app.route("/article/")
+@app.route("/article/<int:a_id>/")
+def article_page(a_id=None):
+    if a_id is not None:
+        return render_template('article.html', a_id=a_id)
+    else:
+        return redirect(url_for('article_page', a_id=1))
+
+
+@app.route("/article/<string:a_id>/")
+def article_page_str(a_id=None):
+    return render_template('article_invalid.html', a_id=a_id)
+
+
+@app.route("/user/", methods=["GET", "POST"])
+def user_page():
+    user_request_contents = [
+        f'Path: {request.environ["PATH_INFO"]}',
+        f'Agent: {request.environ["HTTP_USER_AGENT"]}',
+        f'Cookie: {request.environ["HTTP_COOKIE"]}'
+    ]
+    return render_template(
+        "user.html",
+        user_request_contents=user_request_contents,
+        user_name=request.form.get('f_name') if request.method == 'POST' else None
+    )
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000)
